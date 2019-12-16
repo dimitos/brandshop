e049e7c004af6e5e318426f0f0fa2fd2fa5a704b
dimitos <kurort26@mail.ru>
2019-12-16 22:17:17 +0300

    Связали файлы PHP

---
+++ templates/main.php
@@ -0,0 +1,24 @@
+<div id="promo">
+    <h1 id="promoText">
+        Новый PhoneBrand - это лучший смартфон!
+    </h1>
+</div>
+
+<div id="mainTextWrap">
+    <div id="mainText">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. A accusamus accusantium cupiditate dolore, earum
+        facilis fuga in labore magni natus nisi odit praesentium quam quidem repudiandae sit totam! Doloribus, ipsum
+        mollitia necessitatibus neque quae quia similique voluptas. Dolorem, necessitatibus, quae? Architecto cupiditate
+        delectus eligendi eum in! Consequatur harum ipsam nemo quam quo reprehenderit sed sunt? Exercitationem ipsum
+        maiores non numquam rerum. A amet, ducimus fuga hic iste nostrum unde. Cupiditate, dolorem, velit. Atque cum
+        dolores eius ipsam labore minima, mollitia nesciunt totam. Ab, accusamus blanditiis commodi corporis dicta
+        dolore dolores enim excepturi facere fuga id libero minima quae, qui quisquam quo ullam, veniam! Consequuntur
+        eaque in libero omnis perferendis similique tenetur unde vel? A accusantium amet, culpa cumque dolorum eius,
+        eligendi eveniet minima, nisi nobis nostrum quod saepe sapiente. Ab, accusamus accusantium autem commodi cum
+        dicta distinctio eaque eum exercitationem illum maiores minima neque odit possimus ratione repudiandae sunt
+        ullam! Animi, doloribus nulla? Aspernatur blanditiis exercitationem fugiat harum nesciunt, praesentium
+        temporibus vel. Ab inventore suscipit tempore. Animi blanditiis eius id nobis odio! Animi libero minima
+        necessitatibus neque, possimus repellat voluptatibus. Accusantium, adipisci animi aperiam culpa ducimus
+        eveniet hic labore omnis provident quis reiciendis sapiente similique sint soluta totam vel voluptas.
+    </div>
+</div>
